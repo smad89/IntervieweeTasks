fe14d42c083231a315ab8e16475572004c4e46a9
Sarmad <Sarmad@Sarmad-PC>
2016-02-02 21:11:36 -0500

    Created POJO Class for Customer Document Data

---
+++ DataIntegrationTest/src/main/java/com/interset/DataIntegrationTask/CustomerData.java
@@ -0,0 +1,65 @@
+package com.interset.DataIntegrationTask;
+
+public class CustomerData {
+
+	//initialize your JSON parsed data
+	private Long eventID;
+	private String user, ipAddr, file, activity, timestamp, timeOffset;
+	
+	
+	public Long getEventID() {
+		return eventID;
+	}
+	public void setEventID(Long eventID) {
+		this.eventID = eventID;
+	}
+	public String getUser() {
+		return user;
+	}
+	public void setUser(String user) {
+		this.user = user;
+	}
+	public String getIpAddr() {
+		return ipAddr;
+	}
+	public void setIpAddr(String ipAddr) {
+		this.ipAddr = ipAddr;
+	}
+	public String getFile() {
+		return file;
+	}
+	public void setFile(String file) {
+		this.file = file;
+	}
+	public String getActivity() {
+		return activity;
+	}
+	public void setActivity(String activity) {
+		this.activity = activity;
+	}
+	public String getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(String timestamp) {
+		this.timestamp = timestamp;
+	}
+	public String getTimeOffset() {
+		return timeOffset;
+	}
+	public void setTimeOffset(String timeOffset) {
+		this.timeOffset = timeOffset;
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
--- DataIntegrationTest/src/main/java/com/interset/DataIntegrationTask/TaskRunner.java
+++ DataIntegrationTest/src/main/java/com/interset/DataIntegrationTask/TaskRunner.java
@@ -1,61 +1,81 @@
 package com.interset.DataIntegrationTask;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class TaskRunner {
 
-    public static void main(String args[]) {
+	public static void main(String args[]) {
 
-        // Check arguments
-        if (args.length != 2) {
-            System.out.println("We currently only expect 2 arguments! A path to a JSON file to read, and a path for a CSV file to write.");
-            System.exit(1);
-        }
+		// Check arguments
+		if (args.length != 2) {
+			System.out.println(
+					"We currently only expect 2 arguments! A path to a JSON file to read, and a path for a CSV file to write.");
+			System.exit(1);
+		}
 
-        // Read arguments
-        Path jsonFile = null;
+		// Read arguments
+		Path jsonFile = null;
 
-        try {
-            jsonFile = Paths.get(args[0]);
-        } catch (InvalidPathException e) {
-            System.err.println("Couldn't convert JSON file argument [" + args[0] + "] into a path!");
-            throw e;
-        }
+		try {
+			jsonFile = Paths.get(args[0]);
+		} catch (InvalidPathException e) {
+			System.err.println("Couldn't convert JSON file argument [" + args[0] + "] into a path!");
+			throw e;
+		}
 
-        Path csvFile = null;
+		Path csvFile = null;
 
-        try {
-            csvFile = Paths.get(args[1]);
-        } catch (InvalidPathException e) {
-            System.err.println("Couldn't convert CSV file argument [" + args[1] + "] into a path!");
-            throw e;
-        }
+		try {
+			csvFile = Paths.get(args[1]);
+		} catch (InvalidPathException e) {
+			System.err.println("Couldn't convert CSV file argument [" + args[1] + "] into a path!");
+			throw e;
+		}
 
-        // Validate arguments
-        if (!Files.exists(jsonFile)) {
-            System.err.println("JSON file [" + jsonFile.toString() + "] doesn't exist!");
-            System.exit(1);
-        }
+		// Validate arguments
+		if (!Files.exists(jsonFile)) {
+			System.err.println("JSON file [" + jsonFile.toString() + "] doesn't exist!");
+			System.exit(1);
+		}
 
-        if (!Files.isWritable(csvFile.getParent())) {
-            System.err.println("Can't write to the directory [" + csvFile.getParent().toString() + "] to create the CSV file! Does directory exist?");
-            System.exit(1);
-        }
+		if (!Files.isWritable(csvFile.getParent())) {
+			System.err.println("Can't write to the directory [" + csvFile.getParent().toString()
+					+ "] to create the CSV file! Does directory exist?");
+			System.exit(1);
+		}
 
-        // Create the CSV file
-        System.out.println("Reading file [" + jsonFile.toString() + "], and writing to file [" + csvFile.toString() + "].");
+		// Create the CSV file
+		System.out.println(
+				"Reading file [" + jsonFile.toString() + "], and writing to file [" + csvFile.toString() + "].");
 
-        parseJsonFileAndCreateCsvFile(jsonFile, csvFile);
+		try {
+			parseJsonFileAndCreateCsvFile(jsonFile, csvFile);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-    }
+	}
 
-    public static void parseJsonFileAndCreateCsvFile(Path jsonFile, Path csvFile) {
+	public static void parseJsonFileAndCreateCsvFile(Path jsonFile, Path csvFile) throws IOException {
 
-        //Initialize Jackon and Common CSV Objects 1
-    	
-    }
+		//read JSON file data to String
+        byte[] jsonData = Files.readAllBytes(jsonFile);
+         
+        //instantiate ObjectMapper instance
+        ObjectMapper objMapper = new ObjectMapper();
+        
+        //convert the JSON String into POJO Customer Data
+        CustomerData custData = objMapper.readValue(jsonData, CustomerData.class);
+         
+        System.out.println(custData);
+
+		
+	}
 
 }
